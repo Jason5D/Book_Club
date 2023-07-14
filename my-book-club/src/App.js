eaae8f958e24238a8394c99424e358b11ab88b28
Jason <jason5donoghue@gmail.com>
2023-07-14 11:19:05 +0100

    added basic boiler plate

---
+++ my-book-club/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Navbar from './Navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-book-club/src/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <h2>My Books</h2>
+            <h2>My Friends</h2>
+            <h2>My Recommendations</h2>
+            <h2>Meet</h2>
+        </div>
+    )
+}
